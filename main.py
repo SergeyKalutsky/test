fe25ff1317ee915f46bcc35e166795a03a4feb2b
SergeyKalutsky <skalutsky@gmail.com>
2023-02-25 22:41:50 +0600

    test

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import time
 for i in range(10):
     print('I am super cool, Sergey')
     
+    time.sleep(1)
+    
+    print('I have too!')
+    print('I have too!')
+    print('I have too!!!!')
+    print('****')
     
     time.sleep(1)
-    print('I have too!')
     
     
